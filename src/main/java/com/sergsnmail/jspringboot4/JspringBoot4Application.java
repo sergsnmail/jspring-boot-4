052567817ee31f766b0220c6cc26f1ddbbf4239e
Sergey Pozdeev <sergsnmail@gmail.com>
2021-06-26 12:05:50 +0300

    Initial commit

---
+++ src/main/java/com/sergsnmail/jspringboot4/JspringBoot4Application.java
@@ -0,0 +1,13 @@
+package com.sergsnmail.jspringboot4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspringBoot4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspringBoot4Application.class, args);
+	}
+
+}
